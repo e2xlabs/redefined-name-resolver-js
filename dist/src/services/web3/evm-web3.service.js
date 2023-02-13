eb26af420300c6f92dd3fd2b04e00bcca292dca3
Ivan SOFTWEA <ivan.kuzin@softwea.com>
2023-02-13 18:08:29 +0300

    Fix web3

--- dist/src/services/web3/evm-web3.service.js
+++ dist/src/services/web3/evm-web3.service.js
@@ -1,9 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const web3_1 = require("web3");
+const web3_providers_http_1 = require("web3-providers-http");
 class EvmWeb3Service {
     static getWeb3(nodeLink) {
-        return new web3_1.default(new web3_1.default.providers.HttpProvider(nodeLink));
+        return new web3_1.default(new web3_providers_http_1.HttpProvider(nodeLink));
     }
 }
 exports.default = EvmWeb3Service;
