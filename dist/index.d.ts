8f2387cc757aab1f5896211420d73db0617c2591
Ivan SOFTWEA <ivan.kuzin@softwea.com>
2023-02-13 16:34:53 +0300

    FIX

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "@resolver/redefined.resolver";
-export * from "@resolver/models/types";
-export * from "@resolver/services/abis/redefined-resolver.abi";
-//# sourceMappingURL=index.d.ts.map
