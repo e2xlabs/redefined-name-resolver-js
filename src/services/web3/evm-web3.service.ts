19eabd4e8e38ce860c5325c60e508cc14f6f3b9a
Ivan SOFTWEA <ivan.kuzin@softwea.com>
2023-02-13 18:07:38 +0300

    Fix http-provider

--- src/services/web3/evm-web3.service.ts
+++ src/services/web3/evm-web3.service.ts
@@ -1,8 +1,9 @@
 import Web3 from "web3";
+import { HttpProvider } from "web3-providers-http";
 
 export default class EvmWeb3Service {
 
     static getWeb3(nodeLink: string): Web3 {
-        return new Web3(new Web3.providers.HttpProvider(nodeLink));
+        return new Web3(new HttpProvider(nodeLink));
     }
 }
