51f29611c4fbe49fa613cf8a61463b2eb17b039e
Ivan SOFTWEA <ivan.kuzin@softwea.com>
2023-03-01 12:34:49 +0300

    RED fix

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,7 @@
-const ETH_GOERLI_NODE = "https://nameless-frequent-sea.ethereum-goerli.discover.quiknode.pro/251fd072d4269477a9053b036f131705359808f8/";
-
 export default {
     REDEFINED_EMAIL_RESOLVER_CONTRACT_ADDRESS: "0xf20acFC20174acB8D08AAca523b30E8d54f64CfA",
     REDEFINED_NICKNAME_RESOLVER_CONTRACT_ADDRESS: "0x4B92cB15B1A56acFB53C4956C980D9ACc5809094",
-    ARBITRUM_NODE: ETH_GOERLI_NODE,
+    ARBITRUM_NODE: "https://thrumming-shy-shadow.arbitrum-mainnet.discover.quiknode.pro/8293587af0f7311ba25216d08c72844c9d39fdfe/",
     ETH_NODE: "https://attentive-blue-choice.discover.quiknode.pro/a6d16bf50e42db77cd315b60e2c1f5c2386537e3/",
     BSC_NODE: "https://bsc-dataseed.binance.org",
     ZIL_NODE: "https://evm-api-dev.zilliqa.com",
