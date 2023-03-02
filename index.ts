0e41f287704616dbda1f3649c2eb9c66aba1f2e0
IvanKuzin <ik@e2xlabs.com>
2023-03-02 13:22:28 +0300

    RED-424 export contracts

--- index.ts
+++ index.ts
@@ -3,3 +3,4 @@ export * from "@resolver/models/types";
 export * from "@resolver/services/resolvers/resolver.service";
 export * from "@resolver/services/abis/redefined-email-resolver.abi";
 export * from "@resolver/services/abis/redefined-nickname-resolver.abi";
+export * from "@resolver/redefined-contracts";
--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,14 @@
-export default {
-    REDEFINED_EMAIL_RESOLVER_CONTRACT_ADDRESS: "0xf20acFC20174acB8D08AAca523b30E8d54f64CfA",
-    REDEFINED_NICKNAME_RESOLVER_CONTRACT_ADDRESS: "0x4B92cB15B1A56acFB53C4956C980D9ACc5809094",
+import { CONTRACTS } from "@resolver/redefined-contracts";
+
+export const NODES = {
     ARBITRUM_NODE: "https://thrumming-shy-shadow.arbitrum-mainnet.discover.quiknode.pro/8293587af0f7311ba25216d08c72844c9d39fdfe/",
     ETH_NODE: "https://attentive-blue-choice.discover.quiknode.pro/a6d16bf50e42db77cd315b60e2c1f5c2386537e3/",
     BSC_NODE: "https://bsc-dataseed.binance.org",
     ZIL_NODE: "https://evm-api-dev.zilliqa.com",
     POLYGON_NODE: "https://nameless-small-tree.matic.discover.quiknode.pro/1c4a2e33a0f87bf379039694c7de15aff5e0aaf5/",
+}
+
+export default {
+    ...CONTRACTS,
+    ...NODES,
 }
