1e84373b8fe3d8a8de33e50fd28094706cd4d648
Ivan SOFTWEA <ivan.kuzin@softwea.com>
2023-03-03 13:14:48 +0300

    RED update config fn + fix bad pckg

--- index.ts
+++ index.ts
@@ -3,4 +3,13 @@ export * from "@resolver/models/types";
 export * from "@resolver/services/resolvers/resolver.service";
 export * from "@resolver/services/abis/redefined-email-resolver.abi";
 export * from "@resolver/services/abis/redefined-nickname-resolver.abi";
-export * from "@resolver/redefined-contracts";
+export * from "@resolver/config";
+import config from "@resolver/config"
+
+export function updateConfig(params: { emailContract?: string, nicknameContract?: string }) {
+    if (params.emailContract)
+        config.REDEFINED_EMAIL_RESOLVER_CONTRACT_ADDRESS = params.emailContract;
+    
+    if (params.nicknameContract)
+        config.REDEFINED_NICKNAME_RESOLVER_CONTRACT_ADDRESS = params.nicknameContract;
+}
