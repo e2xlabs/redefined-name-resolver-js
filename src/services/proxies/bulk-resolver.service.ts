fd11f0570aa0d127a790d114d288c8a13e184594
andrey.ulyanov <andrey.ulyanov@softwea.com>
2023-04-19 20:36:39 +0300

    RED-521 remove redudant check

--- src/services/proxies/bulk-resolver.service.ts
+++ src/services/proxies/bulk-resolver.service.ts
@@ -1,6 +1,5 @@
 import { ResolverService } from "@resolver/services/resolvers/resolver.service";
 import { Account, CustomResolverServiceOptions, ResolverVendor } from "@resolver/models/types";
-import { uniqBy } from "lodash";
 
 export class BulkProxy<C extends any, R extends ResolverService> implements ResolverService {
 
@@ -10,9 +9,6 @@ export class BulkProxy<C extends any, R extends ResolverService> implements Reso
     this.configuredResolvers = configs?.map(n => instanceRef(n)) || [];
     this.configuredResolvers.push(instanceRef(undefined));
     this.vendor = this.configuredResolvers[0].vendor;
-    if (uniqBy(this.configuredResolvers, "vendor").length !== 1) {
-      throw Error("Resolver must have only 1 vendor");
-    }
   }
 
   readonly vendor: ResolverVendor = "";
