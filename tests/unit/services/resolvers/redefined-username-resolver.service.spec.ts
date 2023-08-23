91b2fc48969ee925c4abe2ee6fadbf62b572b53c
Kerovazzky <sg@e2xlabs.com>
2023-08-23 07:39:35 +0300

    RED-648 fix review

--- tests/unit/services/resolvers/redefined-username-resolver.service.spec.ts
+++ tests/unit/services/resolvers/redefined-username-resolver.service.spec.ts
@@ -1,6 +1,5 @@
 import config from "@resolver/config";
 import { RedefinedUsernameResolverService } from "@resolver/services/resolvers/redefined-username-resolver.service";
-import { BN } from "ethereumjs-util";
 
 describe('redefined-username-resolver.service', () => {
     const redefinedUsernameResolverService = new RedefinedUsernameResolverService(config.REDEFINED_NODE, true);
