81dcd1732e7b615d404bef8864360bdf5340d3d2
andrey.ulyanov <andrey.ulyanov@e2xlabs.com>
2023-11-10 18:41:33 +0300

    fix(processing timeout request): processing timeout request to rpc

--- src/services/proxies/bulk-resolver.service.ts
+++ src/services/proxies/bulk-resolver.service.ts
@@ -3,68 +3,75 @@ import { Account, CustomResolverServiceOptions, ResolverVendor, ReverseAccount }
 
 export class BulkProxy<C extends any, R extends ResolverService> implements ResolverService {
 
-  readonly vendor: ResolverVendor = "";
+    readonly vendor: ResolverVendor = "";
 
-  private readonly configuredResolvers: R[] = [];
+    private readonly configuredResolvers: R[] = [];
 
-  private readonly allowableErrorMessages = new Map<ResolverVendor, string[]>([
-      ["sid", ["Invalid name", "is not registered", "Invalid address"]],
-      ["ens", ["Cant resolve", "Invalid address"]],
-      ["unstoppable", ["is not registered", "is invalid", "is not supported", "Invalid address"]],
-      ["bonfida", ["is not supported", "Invalid name account provided", "Invalid address"]],
-      ["redefined-email", [
-          "is not registered",
-          "Invalid characters, allowed only lowercase alphanumeric and -_",
-          "No records found for domain",
-          "Unsupported method"
-      ]],
-      ["redefined-username", [
-          "is not registered",
-          "Invalid characters, allowed only lowercase alphanumeric and -_",
-          "No records found for domain",
-          "Name has incorrect length",
-          "Name should be at least 4 characters",
-          "Name should be at most 63 characters",
-          "Name should start with a letter",
-          "Invalid address"
-      ]],
-      ["lens", ["is not supported", "Incorrect domain", "Invalid address"]]
-  ])
+    private readonly allowableErrorMessages = new Map<ResolverVendor, string[]>([
+        ["sid", ["Invalid name", "is not registered", "Invalid address"]],
+        ["ens", ["Cant resolve", "Invalid address"]],
+        ["unstoppable", ["is not registered", "is invalid", "is not supported", "Invalid address"]],
+        ["bonfida", ["is not supported", "Invalid name account provided", "Invalid address"]],
+        ["redefined-email", [
+            "is not registered",
+            "Invalid characters, allowed only lowercase alphanumeric and -_",
+            "No records found for domain",
+            "Unsupported method"
+        ]],
+        ["redefined-username", [
+            "is not registered",
+            "Invalid characters, allowed only lowercase alphanumeric and -_",
+            "No records found for domain",
+            "Name has incorrect length",
+            "Name should be at least 4 characters",
+            "Name should be at most 63 characters",
+            "Name should start with a letter",
+            "Invalid address"
+        ]],
+        ["lens", ["is not supported", "Incorrect domain", "Invalid address"]]
+    ])
 
-  constructor(configs: C[] | undefined, instanceRef: (config: C | undefined) => R) {
-    this.configuredResolvers = configs?.map(n => instanceRef(n)) || [];
-    this.configuredResolvers.push(instanceRef(undefined));
-    this.vendor = this.configuredResolvers[0].vendor;
-  }
-
-  async resolve(domain: string, networks?: string[], options?: CustomResolverServiceOptions): Promise<Account[]> {
-    let lastError: any;
-    for (let resolver of this.configuredResolvers) {
-      try {
-        return await resolver.resolve(domain, networks, options);
-      } catch (e: any) {
-        if (this.allowableErrorMessages.get(this.vendor)?.some(msg => e.message.includes(msg))) {
-          throw e;
-        }
-        lastError = e;
-      }
+    constructor(configs: C[] | undefined, instanceRef: (config: C | undefined) => R) {
+        this.configuredResolvers = configs?.map(n => instanceRef(n)) || [];
+        this.configuredResolvers.push(instanceRef(undefined));
+        this.vendor = this.configuredResolvers[0].vendor;
     }
-    throw lastError
-  }
+
+    private async timeoutPromise<T>(promise: Promise<T>, timeoutMillis: number): Promise<T> {
+        return Promise.race([
+            promise,
+            new Promise<T>((_, reject) => setTimeout(() => reject(new Error("Request execution time exceeded the limit")), timeoutMillis))
+        ]);
+    }
+
+    async resolve(domain: string, networks?: string[], options?: CustomResolverServiceOptions): Promise<Account[]> {
+        let lastError: any;
+        for (let resolver of this.configuredResolvers) {
+            try {
+                return await this.timeoutPromise(resolver.resolve(domain, networks, options), 5000);
+            } catch (e: any) {
+                if (this.allowableErrorMessages.get(this.vendor)?.some(msg => e.message.includes(msg))) {
+                    throw e;
+                }
+                lastError = e;
+            }
+        }
+        throw lastError;
+    }
 
     async reverse(address: string): Promise<ReverseAccount[]> {
-      let lastError: any;
-      for (let resolver of this.configuredResolvers) {
-        try {
-          if(!instanceOfSupportReverse(resolver)) return [];
-          return await resolver.reverse(address);
-        } catch (e: any) {
-          if (this.allowableErrorMessages.get(this.vendor)?.some(msg => e.message.includes(msg))) {
-              throw e;
-          }
-          lastError = e;
+        let lastError: any;
+        for (let resolver of this.configuredResolvers) {
+            try {
+                if (!instanceOfSupportReverse(resolver)) return [];
+                return await this.timeoutPromise(resolver.reverse(address), 5000);
+            } catch (e: any) {
+                if (this.allowableErrorMessages.get(this.vendor)?.some(msg => e.message.includes(msg))) {
+                    throw e;
+                }
+                lastError = e;
+            }
         }
-      }
-      throw lastError
+        throw lastError;
     }
 }
